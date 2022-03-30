3c638591377a63ef09dddeb0e780cd36bcfa1d71
livramateus <livra.matheus@gmail.com>
2022-03-30 20:17:28 -0300

    Added Enums for messages

---
+++ src/Core/Enums/Message.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Livramatheus\PlanetgameBack\Core\Enums;
+
+class Message {
+
+    const DB_ERROR             = "Something went wrong with the database";
+    const UNKNOWN_ERROR        = "Something wrong happened, try again later";
+    const BAD_REQUEST_ERROR    = "Something went wrong with your request";
+    const CREDENTIALS_ERROR    = "You don't have the credentials for this operation";
+    const MISSING_PARAMS_ERROR = "Missing parameters or actions";
+    const ITM_NOT_FOUND_NOTICE = "Notice: Item Not Found";
+
+}
